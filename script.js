c0f4cb094d66421a55486de7b588326fefabf1ad
tapas-code <tapas.code247@gmail.com>
2023-01-19 12:23:18 +0530

    Commit changes Version#2.0
    Updated the JS and added a "NEW GAME" button in html.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let cards = [randomCard(), randomCard()]
-let sum=cards[0]+cards[1]
+let isAlive = false
+let isBlackJack = false
 let messageEl = document.querySelector("#message-p")
 let cardEl = document.querySelector("#cards-p")
 let sumEl = document.querySelector("#sum-p")
-let isAlive = false
-let isBlackJack = false
+let cards = []
+let sum = 0
 
 function randomCard(){
     let card=Math.floor((Math.random()*13)+1)
@@ -15,6 +15,8 @@ function randomCard(){
     }else return card
 }
 function startGame(){
+    cards = [randomCard(), randomCard()]
+    sum = cards[0]+cards[1]
     isAlive = true
     cardEl.textContent = "Cards: " + cards
     sumEl.textContent = "Sum: " + sum
@@ -25,12 +27,42 @@ function renderGame(){
         messageEl.textContent = "Draw a card."
     }else if(sum===21){
         messageEl.textContent = "Hoorah! It's a Blackjack."
-        isBlackJack = true
     }else{
         messageEl.textContent = "Try your luck next time."
-        isAlive = false
     }
 }
 function generateCard(){
-    messageEl.textContent = "Function under maintainence."
+    if(sum<21 && isAlive==true){
+        let newCard = randomCard()
+        sum+=newCard
+        cards.push(newCard)
+        cardEl.textContent = "Cards: " + cards
+        sumEl.textContent = "Sum: " + sum
+        renderGame()
+    }
+    else if(sum>21){
+        alert("You Lost! Start a New Game.")
+    }
+    else if(sum===21){
+        alert("You Won! Start a New Game.")
+    }
+    else if(isAlive==false){
+        alert("Click on START GAME.")
+    }
+}
+function newGame(){
+    if(sum>=21){
+        messageEl.textContent = "Want to play a round?"
+        cards.splice(0, cards.length);
+        sum=0
+        cardEl.textContent = "Cards: " 
+        sumEl.textContent = "Sum: " 
+        startGame()
+    }
+    else if(isAlive==false){
+        alert("Click on START GAME.")
+    }
+    else {
+        alert("Finish the current round.")
+    }
 }
